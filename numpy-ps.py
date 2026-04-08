7c97ccef695dabb56e8fed9d08f326818b6ea66f
Perkonos <oleksandra.perkonos.25@kse.org.ua>
2026-04-08 14:58:40 +0300

    ...

--- numpy-ps.py
+++ numpy-ps.py
@@ -9,7 +9,9 @@ likes = np.array([1200, 3000, 800, 4100])
 comments = np.array([150, 400, 50, 600])
 
 # TODO:
-
+total_interaction =likes + comments
+engagement_rate = (total_interaction / views) *100
+print(f"...:{np.round(engagement_rate)}")
 
 
 # Guided Practice 1
@@ -17,7 +19,8 @@ rpm = np.array([1.5, 2.0, 0.8, 2.5])
 production_cost = np.array([10, 30, 5, 50])
 
 # TODO:
-
+money = views//1000 * rpm - production_cost
+print(np.max(money))
 
 
 # Live Coding 2
@@ -28,8 +31,13 @@ cpu_load = np.array([
 ])
 
 # TODO:
-
-
+mean_load = np.mean(cpu_load, axis=1)
+print("mean load", mean_load)
+absolute_max_server = np.max(cpu_load, axis = 1)
+print(absolute_max_server)
+critical_loads = cpu_load[cpu_load > 50]
+print(critical_loads)
+print(critical_loads.size)
 
 
 
@@ -42,6 +50,15 @@ pings = np.array([
 ])
 
 # TODO:
+min_time = np.min(pings, axis=1)
+print(min_time)
+print(np.mean(pings))
+big_ping = pings[pings>200]
+print(big_ping)
+print(big_ping.size)
+print(big_ping.size/pings.size*100)
+
+
 
 
 

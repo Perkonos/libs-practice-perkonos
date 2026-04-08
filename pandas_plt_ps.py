e88734724094aff976046a74a2777edd7a08c636
Perkonos <oleksandra.perkonos.25@kse.org.ua>
2026-04-08 14:58:17 +0300

    ...

--- pandas_plt_ps.py
+++ pandas_plt_ps.py
@@ -1,5 +1,6 @@
 import kagglehub
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # kagglehub.dataset_download("alitaqishah/global-mental-health-crisis-index-2026", output_dir="datasets", force_download=True)
 df = pd.read_csv("datasets/Global_Mental_Health_Crisis_Index_2026.csv")
@@ -14,10 +15,29 @@ df = pd.read_csv("datasets/Global_Mental_Health_Crisis_Index_2026.csv")
 # print(df[['country', 'millions_rate']])
 
 
-mask = (df['gdp_per_capita_usd'] >= 40000) & (df['mh_system_score'] < 5)
-df["lack_of_care_mln"] = df["population_millions"]*df["treatment_gap_pct"]/100
-hiu = df[mask]
-print(hiu[['lack_of_care_mln', 'country']])
+# mask = (df['gdp_per_capita_usd'] >= 40000) & (df['mh_system_score'] < 5)
+# df["lack_of_care_mln"] = df["population_millions"]*df["treatment_gap_pct"]/100
+# hiu = df[mask]
+# print(hiu[['lack_of_care_mln', 'country']])
 
 
+# top5 = df.nlargest(5, 'depression_pct')
+# plt.figure(figsize=(6,7))
+# plt.plot(top5['country'], top5['depression_pct'],
+# marker='o', label='depression', linewidth=2)
+# plt.plot(top5['country'], top5['anxiety_pct'], marker='s', label='anxiety')
+# plt.title('anxiety vs depression (%)')
+# plt.xlabel('countrys')
+# plt.ylabel('%')
+# plt.legend()
+#
+#
+# plt.show()
+plt.figure(figsize=(7,6))
+plt.title("mh vs sm")
+plt.xlabel("hr in sm")
+plt.ylabel("mh")
+plt.scatter(df["social_media_hours_daily"], df["youth_mh_crisis_score"],
+            marker="o", s=100, color='red', alpha=0.5)
+plt.show()
 
